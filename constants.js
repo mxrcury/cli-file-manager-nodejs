a7499f937a7386cac7db1211bdb90c13f2303950
Dmytro <demetrgonchar@gmail.com>
2022-12-08 21:28:38 +0200

    implement logic of other commands

--- constants.js
+++ constants.js
@@ -2,7 +2,24 @@ export const COMMANDS = {
     CD:'cd',
     UP:'up',
     EXIT:'.exit',
-    LS:'ls',      
+    LS:'ls',
+    CAT:'cat',
+    ADD:'add',
+    RN:'rn',
+    CP:'cp',
+    MV:'mv',
+    RM:'rm',
+    OS:'os',
+    HASH:'hash',
+    COMPRESS:'compress',
+    DECOMPRESS: 'decompress'
+}
+export const OPTIONS = {
+    EOL:'--EOL',
+    CPUS:'--cpus',
+    HOMEDIR:'--homedir',
+    USERNAME:'--username',
+    ARCH:'--architecture'
 }
 
 
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import { exit, stdin, stdout, chdir, stderr, cwd } from 'node:process'
-import process from 'node:process'
 import { parseArgs } from './utils.js'
-import { COMMANDS, DIRECTORY, FILE } from './constants.js'
-import { dirname } from 'node:path'
-import { readdir } from 'node:fs/promises'
+import { COMMANDS, DIRECTORY, FILE, OPTIONS } from './constants.js'
+import { readdir, appendFile, rename, rm, rmdir, readFile } from 'node:fs/promises'
+import { createReadStream, createWriteStream } from 'node:fs'
+import path, { join } from 'node:path'
+import { pipeline } from 'node:stream/promises'
+import os from 'node:os'
+import { createHash } from 'node:crypto'
+import { createBrotliCompress, createBrotliDecompress } from 'node:zlib'
 
 const main = async () => {
-    let commandValueStart;
-    let commandValueEnd;
+    // let commandValueStart;
+    // let commandValueEnd;
     let commandValue;
     // !! after make command for changing directory ??
     // User can't go upper than root directory (e.g. on Windows it's current local drive root). If user tries to do so, current working directory doesn't change
@@ -20,24 +24,35 @@ const main = async () => {
     stdout.write(`Welcome to the File Manager, ${username}!\n`);
 
     stdin.on("data", async (data) => {
-      const command = data.toString().trim().toLowerCase();
+      const fullCommand = data.toString().trim().toLowerCase()
+      const commandName = fullCommand.slice(0,fullCommand.indexOf(' '))
+      const currentDirectory = cwd()
+      if (fullCommand.length > commandName.length) {
+      commandValue = fullCommand.slice(
+          fullCommand.indexOf(" ") + 1,
+          fullCommand.length
+        );
+      } else {
+        commandValue = "";
+      }
+      if(!commandName.trim().length){
+        // try to make this <error> reusable | refactor
+        stdout.write("Invalid input\n");
+        return;
+      }
 
-
-      if (command === COMMANDS.UP) {
+      if (fullCommand === COMMANDS.UP) {
         const upperDirectory = "../";
         chdir(upperDirectory);
         return;
       }
-      if (command === COMMANDS.EXIT) {
+      if (fullCommand === COMMANDS.EXIT) {
         stdout.write(
           `Thank you for using File Manager, ${username}, goodbye!\n`
         );
         exit();
       }
-      if (command.startsWith(COMMANDS.CD)) {
-        commandValueStart = COMMANDS.CD.length + 1;
-        commandValueEnd = command.length;
-        commandValue = command.slice(commandValueStart, commandValueEnd);
+      if (fullCommand.startsWith(COMMANDS.CD)) {
         try {
           chdir(commandValue);
         } catch (error) {
@@ -45,22 +60,161 @@ const main = async () => {
         }
         return;
       }
-      if(command === COMMANDS.LS){
-        const currentDirectory = cwd()
+      if(fullCommand === COMMANDS.LS){
         const files = await readdir(currentDirectory,{withFileTypes:true})
         // TODO:Change -tabledFiles- variable name
         const tabledFiles = files.map(file =>{
-            // console.log(file)
             return {Name:file.name,Type:file.isDirectory() ? DIRECTORY : FILE}
         })
-        // stdout.write(tabledFiles.toString())
         console.table(tabledFiles)
         return;
-        // stdout.write(files)
       }
       // ---- COMMANDS WITH FILES ----
+      if(fullCommand.startsWith(COMMANDS.CAT)){
+        const filePath = commandValue
+        const readStream = createReadStream(filePath)
+        readStream.on("readable",()=>{
+          let data = readStream.read()
+          if(data !== null){
+            data = data.toString()
+            stdout.write(`${data}\n`)
+          }
+        })
+        return;        
+      }
+      if(fullCommand.startsWith(COMMANDS.ADD)){
+        const pathToFile = join(currentDirectory,commandValue)
+        const emptyData = ''
+        try {
+          await appendFile(pathToFile,emptyData)
+          return;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      if(fullCommand.startsWith(COMMANDS.RN)){
+          const oldFileName = commandValue.slice(0,commandValue.indexOf(' ')) 
+          const newFileName = commandValue.slice(commandValue.indexOf(' ') + 1, commandValue.length)
+          const oldFilePath = join(currentDirectory,oldFileName)
+          const newFilePath = join(currentDirectory,newFileName)
+        try {
+            await rename(oldFilePath, newFilePath)
+            return;
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      if(fullCommand.startsWith(COMMANDS.CP)){
+          const fileName = commandValue.slice(0,commandValue.indexOf(' ')) 
+          const copiedFileName = commandValue.slice(commandValue.indexOf(' ') + 1, commandValue.length)
+          const filePath = join(currentDirectory,fileName)
+          const copiedFileNewPath = join(currentDirectory,copiedFileName)
 
+          const readStream = createReadStream(filePath)
+          const writeStream = createWriteStream(copiedFileNewPath)
+          try {
+              pipeline(readStream, writeStream)
+            // await copyFile(filePath, newFilePath)
+            return;
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      if(fullCommand.startsWith(COMMANDS.MV)){
+        const fileName = commandValue.slice(0, commandValue.indexOf(" "));
+        const copiedDirectory = commandValue.slice(commandValue.indexOf(' ') + 1,commandValue.length)
+        const copiedFileName = fileName
+        const filePath = join(currentDirectory, fileName);
+        const copiedFileNewPath = join(currentDirectory,copiedDirectory,copiedFileName);
+        
+        const readStream = createReadStream(filePath);
+        const writeStream = createWriteStream(copiedFileNewPath);
+        try {
+          await pipeline(readStream, writeStream);
+          await rm(filePath)
+          return;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      if(fullCommand.startsWith(COMMANDS.RM)){
+        const filePath = join(currentDirectory,commandValue)
+        try {
+          // const isDirectory = await opendir(filePath) 
+          await rm(filePath)
+          return;
+        } catch (error) {
+          // ?? Need to fix check if it's directory but not file
+          await rmdir(filePath)
+          // console.log(error)
+          return;
+        }
+      }
+      if (
+        fullCommand.startsWith(COMMANDS.OS) &&
+        commandValue.toUpperCase().startsWith(OPTIONS.EOL)
+      ) {
+        const eolINfo = os.EOL
+        stdout.write(`${JSON.stringify(eolINfo)}\n`);
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.OS) && commandValue.startsWith(OPTIONS.CPUS)){
+        const cpus = os.cpus().map(cpu=>cpu.model)
+        stdout.write(`Amount of CPUS:${cpus.length}\n`)
+        stdout.write(`${cpus.join('\n')}\n`)
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.OS) && commandValue.startsWith(OPTIONS.HOMEDIR)){
+        const homedir = os.homedir()
+        stdout.write(`${homedir}\n`)
+        return;
+      }
 
+      if(fullCommand.startsWith(COMMANDS.OS)&& commandValue.startsWith(OPTIONS.USERNAME)){
+        const currentUserName = os.userInfo()
+        stdout.write(`${currentUserName.username}\n`);
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.OS) && commandValue.startsWith(OPTIONS.ARCH)){
+        const architecture = os.arch()
+        stdout.write(`${architecture}\n`)
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.HASH)){
+        const SHA256 = 'sha256'
+        const HEX = 'hex'
+        const filePath = join(currentDirectory,commandValue)
+        const hash = createHash(SHA256)
+        const file = await readFile(filePath)
+        hash.update(file)
+        const hashInHex = hash.digest(HEX)
+        stdout.write(`${hashInHex}\n`);
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.COMPRESS)){
+        
+        console.log(commandValue)
+        const file = commandValue.slice(0, commandValue.indexOf(' ')) 
+        const compressedFile = commandValue.slice(commandValue.indexOf(' ') + 1, commandValue.length)
+        const filePath = join(currentDirectory, file)
+        const compressedFilePath = join(currentDirectory, compressedFile)
+        const readStream = createReadStream(filePath)
+        const brotliCompress = createBrotliCompress()
+        const writeStream = createWriteStream(compressedFilePath)
+        await pipeline(readStream,brotliCompress, writeStream)
+        return;
+      }
+      if(fullCommand.startsWith(COMMANDS.DECOMPRESS)){
+        const compressedFile = commandValue.slice(0, commandValue.indexOf(' ')) 
+        const decompressedFile = commandValue.slice(commandValue.indexOf(' ') + 1, commandValue.length) 
+        const compressedFilePath = join(currentDirectory, compressedFile)
+        const decompressedFilePath = join(currentDirectory, decompressedFile)
+        const readStream = createReadStream(compressedFilePath)
+        const brotliDecompress = createBrotliDecompress()
+        const writeStream = createWriteStream(decompressedFilePath)
+        await pipeline(readStream,brotliDecompress,writeStream)
+        return;
+      }
       // validation if no command exists
       stdout.write("Invalid input\n");
     });
